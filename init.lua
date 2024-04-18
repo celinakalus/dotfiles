b5832691f6860ca70492e55dbd9404ad531904e1
Celina Sophie Kalus <hello@celinakalus.de>
2024-04-18 15:05:39 +0200

    Add telescope and treesitter
    Signed-off-by: Celina Sophie Kalus <hello@celinakalus.de>

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+local telescope_builtin = require('telescope.builtin')
+
 vim.opt.listchars = {
 	eol = '↵',
 	tab = '▻ ',
@@ -7,6 +9,13 @@ vim.opt.list = true
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.autoindent = true
 
 vim.wo.number = true
+
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
 
