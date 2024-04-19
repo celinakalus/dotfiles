ab67421ce00f07b08f6af98ed0e2077bff4c6607
Celina Sophie Kalus <hello@celinakalus.de>
2024-04-19 10:41:03 +0200

    Add new keybinds for window navigation
    Signed-off-by: Celina Sophie Kalus <hello@celinakalus.de>

--- init.lua
+++ init.lua
@@ -81,6 +81,11 @@ vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
 
 vim.keymap.set('n', '<leader>gg', ':Flog<CR>', {})
 
+vim.keymap.set('n', '<C-h>', '<C-w>h', {})
+vim.keymap.set('n', '<C-j>', '<C-w>j', {})
+vim.keymap.set('n', '<C-k>', '<C-w>k', {})
+vim.keymap.set('n', '<C-l>', '<C-w>l', {})
+
 vim.g.gitblame_schedule_event = CursorHold
 vim.g.gitblame_clear_event = CursorHoldI
 
