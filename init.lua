d49b19daeb23e2e5b4793cb7c6e80ff41b58b759
Celina Sophie Kalus <hello@celinakalus.de>
2024-09-05 17:49:17 +0200

    init.lua: Add command for renaming based on LSP
    Signed-off-by: Celina Sophie Kalus <hello@celinakalus.de>

--- init.lua
+++ init.lua
@@ -54,6 +54,7 @@ for _, server in ipairs(servers) do
 			local opts = { noremap=true, silent=true, buffer = bufnr }
 			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+			vim.keymap.set('n', '<leader>re', vim.lsp.buf.rename, {})
 		end
 	}
 end
