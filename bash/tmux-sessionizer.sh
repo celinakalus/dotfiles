2248ef39bc23cf6bc217e8b24a6f10a1df556a16
Celina Sophie Kalus <hello@celinakalus.de>
2024-09-09 10:32:19 +0200

    bash: Update tmux-sessionizer
    Optimized it towards my personal workflow and needs. Newly created
    sessions that were created from inside tmux were not immediately
    attached too, which I expect to occur quite often in my workflow.
    
    Signed-off-by: Celina Sophie Kalus <hello@celinakalus.de>

--- bash/tmux-sessionizer.sh
+++ bash/tmux-sessionizer.sh
@@ -14,12 +14,12 @@ tmux_sessionizer() {
 	fi
 
 	selected_name=$(basename "$selected" | tr . _)
-	tmux_running=$(pgrep tmux)
 
-	if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-		tmux new-session -s $selected_name -c $selected
-	elif ! tmux has-session -t=$selected_name 2> /dev/null; then
+	if [[ -z $TMUX ]]; then
+		tmux new-session -As $selected_name -c $selected
+	elif ! tmux has-session -t $selected_name 2> /dev/null; then
 		tmux new-session -ds $selected_name -c $selected
+		tmux switch-client -t $selected_name
 	else
 		tmux switch-client -t $selected_name
 	fi
