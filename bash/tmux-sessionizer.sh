52766ec2d7f1a0dfb088ee8bd56b390d00578507
Celina Sophie Kalus <hello@celinakalus.de>
2025-02-09 15:38:29 +0100

    bash: tmux-sessionizer: Load nix shell if available
    Signed-off-by: Celina Sophie Kalus <hello@celinakalus.de>

--- bash/tmux-sessionizer.sh
+++ bash/tmux-sessionizer.sh
@@ -16,11 +16,19 @@ tmux_sessionizer() {
 	selected_name=$(basename "$selected" | tr . _)
 
 	if [[ -z $TMUX ]]; then
-		tmux new-session -As $selected_name -c $selected
+		TMUX_CALL="tmux new-session -As $selected_name -c $selected"
 	elif ! tmux has-session -t $selected_name 2> /dev/null; then
-		tmux new-session -ds $selected_name -c $selected
-		tmux switch-client -t $selected_name
+		TMUX_CALL="tmux new-session -ds $selected_name -c $selected && tmux switch-client -t $selected_name"
 	else
-		tmux switch-client -t $selected_name
+		TMUX_CALL="tmux switch-client -t $selected_name"
+	fi
+
+	if which nix 2>/dev/null >/dev/null && ([ -f $selected/shell.nix ] || [ -f $selected/default.nix ]); then
+		PWD_PRE_NIX_DEVELOP="$PWD"
+		cd $selected
+		nix develop -c bash -c "SHELL=$SHELL $TMUX_CALL"
+		cd $PWD_PRE_NIX_DEVELOP
+	else
+		$TMUX_CALL
 	fi
 }
