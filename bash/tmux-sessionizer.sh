a3ee84c14996b32feb8857f1977cedb47293f5d2
Celina Sophie Kalus <hello@celinakalus.de>
2025-03-18 11:16:10 +0100

    bash: tmux-sessionizer: Revert nix shell loading
    This system caused a lot of trouble in my workflow; Seems like it is not
    yet ready for production. It is more important that I'm able to quickly
    switch around between tmux sessions.
    
    This reverts commit 52766ec2d7f1a0dfb088ee8bd56b390d00578507.
    This reverts commit 742848cbb2e2faf16efba9fe9dc7a5feae68bbe5.

--- bash/tmux-sessionizer.sh
+++ bash/tmux-sessionizer.sh
@@ -16,19 +16,11 @@ tmux_sessionizer() {
 	selected_name=$(basename "$selected" | tr . _)
 
 	if [[ -z $TMUX ]]; then
-		TMUX_CALL="tmux new-session -As $selected_name -c $selected"
+		tmux new-session -As $selected_name -c $selected
 	elif ! tmux has-session -t $selected_name 2> /dev/null; then
-		TMUX_CALL="tmux new-session -ds $selected_name -c $selected; tmux switch-client -t $selected_name"
+		tmux new-session -ds $selected_name -c $selected
+		tmux switch-client -t $selected_name
 	else
-		TMUX_CALL="tmux switch-client -t $selected_name"
-	fi
-
-	if which nix 2>/dev/null >/dev/null && ([ -f $selected/shell.nix ] || [ -f $selected/default.nix ]); then
-		PWD_PRE_NIX_DEVELOP="$PWD"
-		cd $selected
-		nix develop -c bash -c "SHELL=$SHELL $TMUX_CALL"
-		cd $PWD_PRE_NIX_DEVELOP
-	else
-		$TMUX_CALL
+		tmux switch-client -t $selected_name
 	fi
 }
